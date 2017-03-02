16ad7d5ed0476206c21ab415ba9b325fbbf25fc6
Manny <egalli64@gmail.com>
2017-03-02 11:00:26 +0100

    CodeEval Endianness

---
+++ ce/c015.py
@@ -0,0 +1,9 @@
+"""
+CodeEval Endianness
+author: Manny egalli64@gmail.com
+info: http://thisthread.blogspot.com/
+      https://www.codeeval.com/open_challenges/15/
+"""
+import sys
+
+print('{}Endian'.format(sys.byteorder.capitalize()))
