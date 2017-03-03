2b87b63715ad3c584bca09f977981fd4acc17ee8
Emanuele Galli <egalli64@gmail.com>
2017-03-03 22:42:17 +0100

    CodeEval Odd Numbers

---
+++ ce/c025.py
@@ -0,0 +1,8 @@
+"""
+CodeEval Odd Numbers
+author: Manny egalli64@gmail.com
+info: http://thisthread.blogspot.com/
+      https://www.codeeval.com/open_challenges/25/
+"""
+for i in range(1, 100, 2):
+    print(i)
