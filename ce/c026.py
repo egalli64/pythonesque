d7e0efac437e693e1a0352b41a8749b2d423f0be
Emanuele Galli <egalli64@gmail.com>
2017-03-03 22:47:57 +0100

    CodeEval File Size

---
+++ ce/c026.py
@@ -0,0 +1,10 @@
+"""
+CodeEval File Size
+author: Manny egalli64@gmail.com
+info: http://thisthread.blogspot.com/
+      https://www.codeeval.com/open_challenges/26/
+"""
+import sys
+import os
+
+print(os.path.getsize(sys.argv[1]))
