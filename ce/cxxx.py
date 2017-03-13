413cd004308f6aa6af0473f97847405befc24c3a
Manny <egalli64@gmail.com>
2017-03-13 18:09:01 +0100

    Open file using with-as statement

--- ce/cxxx.py
+++ ce/cxxx.py
@@ -12,9 +12,8 @@ def solution(line):
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
-        test_cases = open(sys.argv[1], 'r')
-        for test in test_cases:
-            print(solution(test.rstrip('\n')))
-        test_cases.close()
+        with open(sys.argv[1], 'r') as file:
+            for test in file:
+                print(solution(test.rstrip('\n')))
     else:
         print('Data filename expected as argument!')
