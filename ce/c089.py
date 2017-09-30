e9a7333c5c1dce8329b64d968d2dd1e0c3847ed5
Emanuele Galli <egalli64@gmail.com>
2017-09-30 10:54:53 +0200

    http://thisthread.blogspot.com/2017/09/codeeval-pass-triangle.html CodeEval Pass Triangle

---
+++ ce/c089.py
@@ -0,0 +1,25 @@
+"""
+CodeEval Pass Triangle
+author: Manny egalli64@gmail.com
+info: http://thisthread.blogspot.com/
+      https://www.codeeval.com/open_challenges/89/
+"""
+import sys
+
+
+def solution(data):
+    for i in range(len(data) - 1, 0, -1):
+        for j in range(len(data[i])-1):
+            data[i-1][j] += max(data[i][j], data[i][j+1])
+    return data[0][0]
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        with open(sys.argv[1], 'r') as file:
+            values = []
+            for test in file:
+                values.append([int(x) for x in test.split()])
+            print(solution(values))
+    else:
+        print('Data filename expected as argument!')
