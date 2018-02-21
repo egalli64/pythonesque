33b178c790d4add059fde579b99f47d4e3c6a4e8
Manny <egalli64@gmail.com>
2018-02-21 10:46:38 +0100

    http://thisthread.blogspot.com/2018/02/money-change-again.html Money Change Again

---
+++ algs200x/w5/a_money_change.py
@@ -0,0 +1,40 @@
+"""
+Money Change Again
+
+author: Manny egalli64@gmail.com
+info: http://thisthread.blogspot.com/
+      https://www.edx.org/course/algorithmic-design-techniques-uc-san-diegox-algs200x
+      week 5 - Dynamic Programming 1
+"""
+DENOMINATIONS = [1, 3, 4]
+
+
+# recursive approach
+def solution_naive(target):
+    if target == 0:
+        return 0
+
+    result = target  # assuming 1 is among the available denominations
+    for coin in DENOMINATIONS:
+        if target >= coin:
+            tentative = solution_naive(target - coin) + 1
+            result = min(result, tentative)
+
+    return result
+
+
+def solution_dp(target):
+    cache = [0] * (target + 1)
+
+    for i in range(1, target + 1):
+        cache[i] = cache[i-1] + 1  # assuming DENOMINATIONS[0] == 1
+        for coin in DENOMINATIONS[1:]:
+            if coin <= i:
+                other = cache[i-coin] + 1
+                cache[i] = min(cache[i], other)
+
+    return cache[-1]
+
+
+if __name__ == '__main__':
+    print(solution_naive(int(input())))
---
+++ algs200x/w5/a_money_change_test.py
@@ -0,0 +1,42 @@
+"""
+Money Change Again
+
+author: Manny egalli64@gmail.com
+info: http://thisthread.blogspot.com/
+      https://www.edx.org/course/algorithmic-design-techniques-uc-san-diegox-algs200x
+      week 5 - Dynamic Programming 1
+"""
+import unittest
+
+from algs200x.w5.a_money_change import solution_naive, solution_dp
+
+
+class TestSolution(unittest.TestCase):
+    def test_naive_0(self):
+        self.assertEqual(2, solution_naive(6))
+
+    def test_naive_1(self):
+        self.assertEqual(2, solution_naive(2))
+
+    # way too slow
+    # def test_naive_2(self):
+    #     self.assertEqual(9, solution_naive(34))
+
+    def test_naive_slow(self):
+        self.assertEqual(7, solution_naive(28))
+
+    def test_dp_0(self):
+        self.assertEqual(2, solution_dp(6))
+
+    def test_dp_1(self):
+        self.assertEqual(2, solution_dp(2))
+
+    def test_dp_2(self):
+        self.assertEqual(9, solution_dp(34))
+
+    def test_dp_quiz(self):
+        self.assertEqual(238, solution_dp(950))
+
+
+if __name__ == '__main__':
+    unittest.main()
