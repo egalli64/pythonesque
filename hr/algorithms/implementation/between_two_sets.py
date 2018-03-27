07a91ec2fd4d0027e9730f6f6663db4ed8ac565e
Manny <egalli64@gmail.com>
2018-03-27 11:28:28 +0200

    HackerRank Python Implementation Between Two Sets

---
+++ hr/algorithms/implementation/between_two_sets.py
@@ -0,0 +1,28 @@
+"""
+HackerRank Python Implementation Between Two Sets
+
+author: Manny egalli64@gmail.com
+info: http://thisthread.blogspot.com/
+      https://www.hackerrank.com/challenges/between-two-sets/problem
+
+Given two arrays of integers.
+Return the number of integers such as:
+- The elements of the first array are all factors of it
+- It is a factor of all elements of the second array
+"""
+
+
+def solution(minors, majors):
+    result = 0
+    for candidate in range(max(minors), min(majors) + 1):
+        if all(candidate % below == 0 for below in minors) and all(above % candidate == 0 for above in majors):
+            result += 1
+    return result
+
+
+if __name__ == '__main__':
+    input()  # discard header
+    lhs = list(map(int, input().split()))
+    rhs = list(map(int, input().split()))
+
+    print(solution(lhs, rhs))
---
+++ hr/algorithms/implementation/between_two_sets_test.py
@@ -0,0 +1,21 @@
+"""
+HackerRank Python Implementation Between Two Sets
+
+author: Manny egalli64@gmail.com
+info: http://thisthread.blogspot.com/
+      https://www.hackerrank.com/challenges/between-two-sets/problem
+"""
+
+import unittest
+from hr.algorithms.implementation.between_two_sets import solution
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_provided_1(self):
+        self.assertEqual(3, solution([2, 4], [16, 32, 96]))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
