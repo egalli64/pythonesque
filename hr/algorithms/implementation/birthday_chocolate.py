fc647178e2c22842a269b312dbc03990bd1dfc5c
Manny <egalli64@gmail.com>
2018-03-27 13:24:00 +0200

    HackerRank Python Implementation Birthday Chocolate

---
+++ hr/algorithms/implementation/birthday_chocolate.py
@@ -0,0 +1,37 @@
+"""
+HackerRank Python Implementation Birthday Chocolate
+
+author: Manny egalli64@gmail.com
+info: http://thisthread.blogspot.com/
+      https://www.hackerrank.com/challenges/the-birthday-bar/problem
+
+Given
+- a list of N integers
+- day and month 
+Return the number of chops from the list such as:
+- size is month
+- total is day
+"""
+
+
+def solution(data, day, month):
+    result = 0
+
+    partial_sum = sum(data[:month])
+    if partial_sum == day:
+        result += 1
+    for i in range(0, len(data) - month):
+        partial_sum -= data[i]
+        partial_sum += data[i+month]
+        if partial_sum == day:
+            result += 1
+
+    return result
+
+
+if __name__ == '__main__':
+    input()  # discard header
+    items = list(map(int, input().split()))
+    d, m = tuple(map(int, input().split()))
+
+    print(solution(items, d, m))
---
+++ hr/algorithms/implementation/birthday_chocolate_test.py
@@ -0,0 +1,27 @@
+"""
+HackerRank Python Implementation Birthday Chocolate
+
+author: Manny egalli64@gmail.com
+info: http://thisthread.blogspot.com/
+      https://www.hackerrank.com/challenges/the-birthday-bar/problem
+"""
+
+import unittest
+from birthday_chocolate import solution
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_provided_0(self):
+        self.assertEqual(2, solution([1, 2, 1, 3, 2], 3, 2))
+
+    def test_provided_1(self):
+        self.assertEqual(0, solution([1, 1, 1, 1, 1, 1], 3, 2))
+
+    def test_provided_2(self):
+        self.assertEqual(1, solution([4], 4, 1))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
