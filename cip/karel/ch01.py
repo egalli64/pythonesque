7bca7062ba267f0bf75e3156acec6602ec6191de
Emanuele <egalli64@gmail.com>
2023-04-19 13:38:05 +0200

    Meet Karel

---
+++ cip/karel/ch01.py
@@ -0,0 +1,41 @@
+"""
+Karel the Robot - Learns Python
+
+Source: https://compedu.stanford.edu/karel-reader/docs/python/en/intro.html
+My notes: https://github.com/egalli64/pythonesque/cip/karel
+
+Chapter 1: Introducing Karel the Robot
+https://compedu.stanford.edu/karel-reader/docs/python/en/chapter1.html
+"""
+# see https://pypi.org/project/stanfordkarel/
+from stanfordkarel import *
+
+# the user startup function
+def main():
+    # move forward one block, when possible
+    move()
+    # take a beeper from the bag and put it down on the current corner
+    put_beeper()
+
+    move()
+
+    # rotate 90 degrees to the left (counterclockwise)
+    turn_left()
+
+    # going back
+    turn_left()
+    move()
+
+    # pick up one beeper from a corner and stores it in its bag
+    pick_beeper()
+
+    move()
+
+    # reset position
+    turn_left()
+    turn_left()
+
+
+if __name__ == '__main__':
+    # setup of Karel world
+    run_karel_program()
