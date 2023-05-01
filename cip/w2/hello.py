b4b29a3e4b2e916af0c35867c0f208849df50783
Emanuele <egalli64@gmail.com>
2023-05-01 21:26:52 +0200

    hello

---
+++ cip/w2/hello.py
@@ -0,0 +1,16 @@
+"""
+Code in Place 2023 https://codeinplace.stanford.edu/cip3
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Week 2: Hello Name
+- Prompt the user for their name and then says hello to them
+"""
+
+
+def main():
+    name = input("What is your name? ")
+    print("Hello", name)
+
+
+if __name__ == '__main__':
+    main()
