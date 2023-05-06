b73dad6fea7b9804424e0d0de4eeb49ae599d3bc
Emanuele <egalli64@gmail.com>
2023-05-06 18:45:48 +0200

    Mars Weight

---
+++ cip/w3/mars.py
@@ -0,0 +1,25 @@
+"""
+Code in Place 2023 https://codeinplace.stanford.edu/cip3
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Section Week 3: Mars Weight
+- Earthling's weight on Mars is 37.8% of their weight on Earth
+- Write a program that prompts an Earthling to enter their weight on Earth and prints their calculated weight on Mars. 
+"""
+
+MARS_FACTOR = 0.378
+
+
+def main():
+    # 1. ask the user for a weight
+    user_input = input('Enter a weight on Earth: ')
+    # 2. convert user input in a real number
+    earth_weight = float(user_input)
+    # 3. get the mars weight from it
+    mars_weight = earth_weight * MARS_FACTOR
+    # 4. output the result
+    print('The equivalent weight on Mars: ' + str(mars_weight))
+
+
+if __name__ == "__main__":
+    main()
