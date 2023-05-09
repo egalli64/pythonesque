ab4e94d816baa8a3fec0ebecb6141dc0f75aa583
Emanuele <egalli64@gmail.com>
2023-05-09 08:21:07 +0200

    Double It

---
+++ cip/w3/ex_double.py
@@ -0,0 +1,20 @@
+"""
+Code in Place 2023 https://codeinplace.stanford.edu/cip3
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Program: Double It
+--------------------
+Ask the user to enter a number
+Double that number and print out the result until the value is greater than 100
+"""
+
+
+def main():
+    curr_value = int(input("Enter a number: "))
+    while curr_value < 100:
+        curr_value *= 2
+        print(curr_value)
+
+
+if __name__ == '__main__':
+    main()
