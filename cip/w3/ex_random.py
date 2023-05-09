b93815180f18b6b2d6e4b3bf920225848d3d7358
Emanuele <egalli64@gmail.com>
2023-05-09 08:08:01 +0200

    Random Numbers

---
+++ cip/w3/ex_random.py
@@ -0,0 +1,17 @@
+"""
+Code in Place 2023 https://codeinplace.stanford.edu/cip3
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Program: Random Numbers
+--------------------
+Print out ten random numbers in the range 1 to 100, inclusive.
+"""
+import random
+
+def main():
+    for i in range(10):
+        value = random.randint(1, 100)
+        print(value)
+
+if __name__ == '__main__':
+    main()
