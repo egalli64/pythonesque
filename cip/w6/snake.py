cca4fddc263b5b4d01bdfe382b471cf1c2d00330
Emanuele <egalli64@gmail.com>
2023-05-30 08:38:06 +0200

    Milestone #2: Animate

--- cip/w6/snake.py
+++ cip/w6/snake.py
@@ -8,6 +8,11 @@ Milestone #1: Set up the World
 a blue square is the "player" the red square is the "goal". The player and the goal are both 20 pixels by 20 pixels. 
 The player starts in the top left corner of the world.
 The goal could be anywhere, as long as its x and y values are both multiplies of 20 e.g.: (360, 360)
+
+Milestone #2: Animate
+Each time through the animation loop you should move your player 20 pixels (this size of the player) in the direction it is traveling.
+The directions are either left, right, up or down.
+At the start the player should be traveling to the right
 """
 from graphics import Canvas
 import time
@@ -16,19 +21,25 @@ import random
 CANVAS_WIDTH = 400
 CANVAS_HEIGHT = 400
 SIZE = 20
+LAST_X = CANVAS_WIDTH - SIZE
+LAST_Y = CANVAS_HEIGHT - SIZE
 
-# if you make this larger, the game will go slower
 DELAY = 0.1
 
 
 def main():
     canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
 
-    player = (0, 0, 'blue')
-    target = (360, 360, 'red')
+    player = [0, 0, 'blue']
+    target = [360, 360, 'red']
 
-    shape_player = place(canvas, player)
-    shape_target = place(canvas, target)
+    player_id = place(canvas, player)
+    target_id = place(canvas, target)
+
+    while player[0] < LAST_X and player[1] < LAST_Y:
+        time.sleep(DELAY)
+        canvas.move(player_id, SIZE, 0)
+        player[0] += SIZE
 
 
 def place(canvas, top):
