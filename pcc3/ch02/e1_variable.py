2accae87cd1d9c4d74c664a1d1aea274f970b411
Emanuele <egalli64@gmail.com>
2023-06-10 15:09:25 +0200

    variable

---
+++ pcc3/ch02/e1_variable.py
@@ -0,0 +1,11 @@
+"""
+Python Crash Course, Third Edition https://ehmatthes.github.io/pcc_3e/
+My notes: https://github.com/egalli64/pythonesque/pcc3
+
+Chapter 2 - Variables
+"""
+# define a variable
+message = "Hello Dennis!"
+
+# pass a variable to a function
+print(message)
---
+++ pcc3/ch02/e2_change_variable.py
@@ -0,0 +1,13 @@
+"""
+Python Crash Course, Third Edition https://ehmatthes.github.io/pcc_3e/
+My notes: https://github.com/egalli64/pythonesque/pcc3
+
+Chapter 2 - Variables
+"""
+# define a variable
+message = "Hello Dennis!"
+print(message)
+
+# assign a new value to a variable
+message = "Goodbye"
+print(message)
