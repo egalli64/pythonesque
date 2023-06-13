e94d17b33dec063aa89d74fffde24fc5128197c6
Emanuele <egalli64@gmail.com>
2023-06-13 19:55:12 +0200

    for each

---
+++ pcc3/ch04/e1_looping.py
@@ -0,0 +1,14 @@
+"""
+Python Crash Course, Third Edition https://ehmatthes.github.io/pcc_3e/
+My notes: https://github.com/egalli64/pythonesque/pcc3
+
+Chapter 4 - Working With Lists - Looping Through an Entire List
+"""
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+print("Thank you, everyone. That was a great magic show!")
