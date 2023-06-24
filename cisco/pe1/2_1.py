bb26d52b6e937d9898f6a8b09b93c1f176b2c381
Emanuele <egalli64@gmail.com>
2023-06-24 11:48:09 +0200

    2.1

---
+++ cisco/pe1/2_1.py
@@ -0,0 +1,24 @@
+"""
+Cisco Network Academy
+Python Essentials 1: https://skillsforall.com/course/python-essentials-1
+My notes: https://github.com/egalli64/pythonesque/cisco/pe1/
+
+PE1: Module 2 Section 1 "Hello, World!"
+"""
+# escape for newline: \n
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+# no-arg print, to print just a newline
+print()
+print("A single backslash: \\")
+
+# multiple arguments
+print("The itsy bitsy spider", "climbed up", "the waterspout.")
+
+# positional vs keyword arguments, by default end is \n
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+# by default sep is a single blank
+print("My", "name", "is", "Monty", "Python.", sep="-")
+
+# * as string multiplier
+print("Hello" * 3)
