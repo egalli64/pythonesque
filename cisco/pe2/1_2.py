1fc828ff5d9115977180363ed3cac63ce860a9ec
Emanuele <egalli64@gmail.com>
2023-10-27 16:51:20 +0200

    math - trigonometry

---
+++ cisco/pe2/1_2.py
@@ -0,0 +1,22 @@
+"""
+Cisco Network Academy
+Python Essentials 2: https://skillsforall.com/course/python-essentials-2
+My notes: https://github.com/egalli64/pythonesque - cisco/pe2 folder
+
+PE2: Module 1 Section 2 – Selected Python modules (math, random, platform)
+"""
+import math
+
+# each attribute in a module (or class, object)
+for name in dir(math):
+    print(name, end="\t")
+
+# the list with all of them
+print(dir(math))
+
+print("Trigonometry")
+print(f" sin: {math.sin(0)}, cos {math.cos(0)}, tan {math.tan(0)}")
+print(f" arcsin: {math.asin(0)}, arccos {math.acos(1)}, arctan {math.atan(0)}")
+print(f" pi: {math.pi}")
+print(f" radians: {math.radians(90)}")
+print(f" degrees: {math.degrees(math.pi / 2)}")
