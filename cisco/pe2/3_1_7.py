0b5489d4c7a3201e23affc0771ea8caa6ccb178c
Emanuele <egalli64@gmail.com>
2023-10-31 17:58:56 +0100

    OOP

---
+++ cisco/pe2/3_1_7.py
@@ -0,0 +1,19 @@
+"""
+Cisco Network Academy
+Python Essentials 2: https://skillsforall.com/course/python-essentials-2
+My notes: https://github.com/egalli64/pythonesque - cisco/pe2 folder
+
+PE2 Module 3 - Object-Oriented Programming
+ Section 1 – The foundations of OOP
+ 7. Your first class
+ 8. Your first object
+"""
+
+
+class TheSimplestClass:
+    pass
+
+
+my_first_object = TheSimplestClass()
+
+print(my_first_object)
