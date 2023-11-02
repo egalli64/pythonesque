2fc2d291f7f9232abefca9655413b18840fcf62a
Emanuele <egalli64@gmail.com>
2023-11-02 12:14:16 +0100

    reflection

---
+++ cisco/pe2/3_4_3.py
@@ -0,0 +1,36 @@
+"""
+Cisco Network Academy
+Python Essentials 2: https://skillsforall.com/course/python-essentials-2
+My notes: https://github.com/egalli64/pythonesque - cisco/pe2 folder
+
+PE2 Module 3 - Object-Oriented Programming
+ Section 4 – OOP: Methods
+ 3. Reflection and introspection
+"""
+
+
+class MyClass:
+    """An empty class"""
+    pass
+
+
+obj = MyClass()
+obj.a = 1
+obj.b = 2
+obj.i = 3
+obj.ireal = 3.5
+obj.integer = 4
+obj.z = 5
+
+
+def incIntsI(obj):
+    for name in obj.__dict__.keys():
+        if name.startswith('i'):
+            val = getattr(obj, name)
+            if isinstance(val, int):
+                setattr(obj, name, val + 1)
+
+
+print(obj.__dict__)
+incIntsI(obj)
+print(obj.__dict__)
