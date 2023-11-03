4ea01f957a848eca0af45d15557b808228a15af9
Emanuele <egalli64@gmail.com>
2023-11-03 16:54:32 +0100

    DDD

---
+++ cisco/pe2/3_5_9.py
@@ -0,0 +1,56 @@
+"""
+Cisco Network Academy
+Python Essentials 2: https://skillsforall.com/course/python-essentials-2
+My notes: https://github.com/egalli64/pythonesque - cisco/pe2 folder
+
+PE2 Module 3 - Object-Oriented Programming
+ Section 5 – OOP Fundamentals: Inheritance
+ 9. The diamond problem
+"""
+
+
+class A:
+    pass
+
+
+class B(A):
+    pass
+
+
+class C(A):
+    pass
+
+
+class D(B, C):
+    pass
+
+
+d = D()
+print(d)
+
+
+class Top:
+    def m_top(self):
+        print("top")
+
+
+class Middle_Left(Top):
+    def m_middle(self):
+        print("middle_left")
+
+
+class Middle_Right(Top):
+    def m_middle(self):
+        print("middle_right")
+
+
+class Bottom(Middle_Left, Middle_Right):
+    def m_bottom(self):
+        print("bottom")
+
+
+object = Bottom()
+object.m_bottom()
+# following MRO rules, Middle_Left super is called
+object.m_middle()
+object.m_top()
