66b2364fe026e131b9c736d209624e06f8c4e770
Emanuele <egalli64@gmail.com>
2023-11-03 17:06:33 +0100

    try except else finally

---
+++ cisco/pe2/3_6_1.py
@@ -0,0 +1,26 @@
+"""
+Cisco Network Academy
+Python Essentials 2: https://skillsforall.com/course/python-essentials-2
+My notes: https://github.com/egalli64/pythonesque - cisco/pe2 folder
+
+PE2 Module 3 - Object-Oriented Programming
+ Section 6 - Exceptions once again
+ 1. More about exceptions
+"""
+
+
+def reciprocal(n):
+    try:
+        n = 1 / n
+    except ZeroDivisionError:
+        print("Division failed")
+        n = None
+    else:
+        print("Everything went fine")
+    finally:
+        print("It's time to say goodbye")
+        return n
+
+
+print(reciprocal(2))
+print(reciprocal(0))
