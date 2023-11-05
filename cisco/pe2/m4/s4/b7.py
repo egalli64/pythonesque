6ad228d8684b49b5c11a2d84854b403ee43dec49
Emanuele <egalli64@gmail.com>
2023-11-05 20:05:42 +0100

    system

---
+++ cisco/pe2/m4/s4/b7.py
@@ -0,0 +1,13 @@
+"""
+Cisco Network Academy
+Python Essentials 2: https://skillsforall.com/course/python-essentials-2
+My notes: https://github.com/egalli64/pythonesque - cisco/pe2 folder
+
+PE2 Module 4 - Miscellaneous
+ Section 4 - The os module - interacting with the operating system
+ 7. The system() function
+"""
+import os
+
+result = os.system("echo hello")
+print(result)
