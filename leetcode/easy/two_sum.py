4acdd1a10542ee60ce4ae99a5ca4a641bb3330f3
Emanuele <egalli64@gmail.com>
2024-03-12 08:27:30 +0100

    simplify

--- leetcode/easy/two_sum.py
+++ leetcode/easy/two_sum.py
@@ -8,19 +8,17 @@ My solutions: https://github.com/egalli64/pythonesque/leetcode
 from typing import List
 
 
-class Solution:
+def twoSum(nums: List[int], target: int) -> List[int]:
     """
     Given an array and a target, return indices of the two elements that add up to target
 
     Each input would have exactly one solution, and you may not use the same element twice
     """
-
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        matchers = {}
-        for i, num in enumerate(nums):
-            matcher = target - num
-            if matcher in matchers:
-                return matchers[matcher], i
-            else:
-                matchers[num] = i
-        return None
+    matchers = {}
+    for i, num in enumerate(nums):
+        matcher = target - num
+        if matcher in matchers:
+            return matchers[matcher], i
+        else:
+            matchers[num] = i
+    return None
--- leetcode/easy/two_sum_test.py
+++ leetcode/easy/two_sum_test.py
@@ -6,22 +6,18 @@ My solutions: https://github.com/egalli64/pythonesque/leetcode
 """
 
 import unittest
-from two_sum import Solution
+from two_sum import twoSum
 
 
 class TestSum3(unittest.TestCase):
-    def __init__(self, methodName: str = "runTest") -> None:
-        super().__init__(methodName)
-        self.sol = Solution()
-
     def test_given_1(self):
-        self.assertCountEqual(self.sol.twoSum([2, 7, 11, 15], 9), [0, 1])
+        self.assertCountEqual(twoSum([2, 7, 11, 15], 9), [0, 1])
 
     def test_given_2(self):
-        self.assertCountEqual(self.sol.twoSum([3, 2, 4], 6), [1, 2])
+        self.assertCountEqual(twoSum([3, 2, 4], 6), [1, 2])
 
     def test_given_3(self):
-        self.assertCountEqual(self.sol.twoSum([3, 3], 6), [0, 1])
+        self.assertCountEqual(twoSum([3, 3], 6), [0, 1])
 
 
 if __name__ == "__main__":
