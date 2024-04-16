1310d296e9160e6fe79fbc120141877f721f008f
Emanuele <egalli64@gmail.com>
2024-04-16 20:42:18 +0200

    rewiew

--- cip/w1/exercise.py
+++ cip/w1/exercise.py
@@ -1,10 +1,10 @@
 """
-Code in Place 2023 https://codeinplace.stanford.edu/cip3
+Code in Place 2023-2024 https://codeinplace.stanford.edu/cip4
 My notes: https://github.com/egalli64/pythonesque/cip
 
 Section Week 1: Hospital Karel
 - See the standard world hospital_karel.w for the initial setup
-- Karel's job is to walk along the row and build a new hospital in the places marked by each beeper.
+- Karel's job is to walk along the row and build a new hospital in the places marked by each beeper
 - Each hospital should be two columns of three beepers
 """
 
@@ -22,37 +22,50 @@ def main():
 
 def build_hospital():
     """
-    Karel builds a hospital.
-    Pre-condition: Karel is on a beeper, representing a
-        pile of supplies. Karel is facing east.
-    Post-condition: Karel is standing at the bottom
-        of the last column of the hospital, facing east.
+    Karel builds a hospital
+
+    Pre-condition: Karel is on a beeper, representing a pile of supplies. Karel is facing east
+    Post-condition: Karel is standing at the bottom of the last column of the hospital, facing east
     """
-    turn_left()
-    build_section()
-    turn_right()
+    pick_beeper()
+    build_column()
     move()
-    put_beeper()
-    turn_right()
-    build_section()
+    build_column()
+
+
+def build_column():
+    """
+    A hospital is done by two columns sized three
+
+    Precondition: Karel is on a beeper, directed to east
+    Postcondition: Karel is in the original position, the first column is placed
+    """
+    turn_left()
+    for i in range(3):
+        put_beeper()
+        move()
+    back_to_base()
+
+
+def back_to_base():
+    """
+    Move Karel to the base row
+
+    Precondition: Karel is directed north
+    Postcondition: Karel is on the first row, directed east
+    """
+    turn_around()
+    while front_is_clear():
+        move()
     turn_left()
 
 
-def build_section():
+def turn_around():
     """
-    We can see a hospital as two columns made of a beeper plus a two beepers in the same row.
-    A section is this pair of beepers.
-    Precondition: Karel is on a beeper, need to put down a section in its current direction
-    Postcondition: Karel is in the same direction, has moved and placed two beepers
+    Make Karel turn around
     """
-    for i in range(2):
-        move()
-        put_beeper()
-
-
-def turn_right():
-    for i in range(3):
-        turn_left()
+    turn_left()
+    turn_left()
 
 
 if __name__ == "__main__":
