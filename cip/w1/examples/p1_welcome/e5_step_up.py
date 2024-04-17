7b7bf0c0173d30a70868ea92d140c860bd8a9016
Emanuele <egalli64@gmail.com>
2024-04-17 21:46:39 +0200

    Step up

---
+++ cip/w1/examples/p1_welcome/e5_step_up.py
@@ -0,0 +1,39 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Step up: remove a beeper from (2, 1), and put one in (4, 2), beware of walls
+"""
+
+from stanfordkarel import *
+
+
+def main():
+    """
+    Precondition: Karel is in (1, 1) facing East
+    Postcondition: Karel is in (5, 2) facing East
+    """
+    move()
+    pick_beeper()
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    put_beeper()
+    move()
+
+
+def turn_right():
+    """
+    Karel does not know how to turn right!
+    """
+    for i in range(3):
+        turn_left()
+
+
+if __name__ == "__main__":
+    """
+    Run python from the current directory, see the worlds folder
+    """
+    run_karel_program("step_up")
---
+++ cip/w1/examples/p1_welcome/worlds/step_up.w
@@ -0,0 +1,7 @@
+Dimension: (5, 3)
+Wall: (3, 1); east
+Wall: (4, 1); north
+Wall: (5, 1); north
+Beeper: (2, 1); 1
+BeeperBag: 1
+Speed: 0.50
