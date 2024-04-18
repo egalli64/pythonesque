3da721e10e305f68fc0680687d319439e90c2f2b
Emanuele <egalli64@gmail.com>
2024-04-18 19:49:54 +0200

    10s across the board

---
+++ cip/w1/examples/p2_control_flow/e1_10s_across.py
@@ -0,0 +1,33 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+10s across the board: place 10 beepers on each cell on the first row
+"""
+
+from stanfordkarel import *
+
+
+def main():
+    """
+    Fill the current row with 10 beeper in each cell
+
+    Precondition: Karel is in (1, 1), facing east
+    Postcondition: Karel is in the last cell of the row, facing east
+    """
+    put_10_beepers()
+    while front_is_clear():
+        move()
+        put_10_beepers()
+
+
+def put_10_beepers():
+    for i in range(10):
+        put_beeper()
+
+
+if __name__ == "__main__":
+    """
+    Run python from the current directory
+    """
+    run_karel_program("8x1")
