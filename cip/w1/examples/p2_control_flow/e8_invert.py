22af48ca7fcd232aeb88de4d1ad0b02075ed708e
Emanuele <egalli64@gmail.com>
2024-04-19 17:29:17 +0200

    Invert

---
+++ cip/w1/examples/p2_control_flow/e8_invert.py
@@ -0,0 +1,31 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Invert: swap beeper/no beeper for each cell in the current row
+"""
+
+from stanfordkarel import *
+
+
+def main():
+    swap_beeper()
+    while front_is_clear():
+        move()
+        swap_beeper()
+
+
+def swap_beeper():
+    if beepers_present():
+        pick_beeper()
+    else:
+        put_beeper()
+    # the following syntax is not supported in CIP :(
+    # pick_beeper() if beepers_present() else put_beeper()
+
+
+if __name__ == "__main__":
+    """
+    Run python from the current directory
+    """
+    run_karel_program("8x1")
