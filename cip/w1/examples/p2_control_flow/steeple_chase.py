e14c6241a78f768d2397ebedd2b8e2ced17f3d98
Emanuele <egalli64@gmail.com>
2024-04-22 19:27:43 +0200

    Steeple Chase

---
+++ cip/w1/examples/p2_control_flow/steeple_chase.py
@@ -0,0 +1,78 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Steeple Chase: walk on the bottom row for 9 columns. When a wall is met, follow it
+"""
+
+from stanfordkarel import *
+
+
+def main():
+    """
+    Move or jump hurdle for eight times
+    """
+    for i in range(8):
+        if front_is_clear():
+            move()
+        else:
+            jump_hurdle()
+
+
+def jump_hurdle():
+    """
+    Pre-condition:  Facing East at bottom of hurdle
+    Post-condition: Facing East at bottom in next avenue after hurdle
+    """
+    ascend_hurdle()
+    move()
+    descend_hurdle()
+
+
+def ascend_hurdle():
+    """
+    Pre-condition:  Facing East at bottom of hurdle
+    Post-condition: Facing East immediately above hurdle
+    """
+    turn_left()  # Face North
+
+    # While there is a wall on the Eastern side of Karel, move up a row
+    while right_is_blocked():
+        move()
+
+    turn_right()  # Face East
+
+
+def descend_hurdle():
+    """
+    Pre-condition:  Facing East above and immediately after hurdle
+    Post-condition: Facing East at bottom of hurdle
+    """
+    turn_right()  # Face South
+    move_to_wall()  # Move to row 1
+    turn_left()  # Face East
+
+
+def move_to_wall():
+    """
+    Pre-condition:  None
+    Post-condition: Facing first wall in whichever direction Karel was facing previously
+    """
+    while front_is_clear():
+        move()
+
+
+def turn_right():
+    """
+    Pre-condition:  None
+    Post-condition: Karel is facing to the right of whichever direction Karel was facing previously
+    """
+    for i in range(3):
+        turn_left()
+
+
+if __name__ == "__main__":
+    """
+    Run python from the current directory
+    """
+    run_karel_program("steeple_chase")
---
+++ cip/w1/examples/p2_control_flow/worlds/steeple_chase.w
@@ -0,0 +1,4 @@
+Dimension: (9, 10)
+Wall: (4, 1); east
+Wall: (6, 1); east
+Wall: (8, 1); east
