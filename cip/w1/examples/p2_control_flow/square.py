6efaaaa503d9dc9d1dd51effa33b0f763e905ec7
Emanuele <egalli64@gmail.com>
2024-04-23 11:47:34 +0200

    Square

---
+++ cip/w1/examples/p2_control_flow/square.py
@@ -0,0 +1,22 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Square: place 4 beepers in a 2x2 square
+"""
+
+from stanfordkarel import *
+
+
+def main():
+    for i in range(4):
+        put_beeper()
+        move()
+        turn_left()
+
+
+if __name__ == "__main__":
+    """
+    Run python from the current directory
+    """
+    run_karel_program("5x5")
