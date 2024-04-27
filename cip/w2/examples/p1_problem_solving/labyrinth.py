08d6d9cc5059576e14e550836b75e0c8b285b0cc
Emanuele <egalli64@gmail.com>
2024-04-27 20:32:48 +0200

    Labyrinth

---
+++ cip/w2/examples/p1_problem_solving/labyrinth.py
@@ -0,0 +1,40 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Labyrinth: Karel is in one dead end, move it to the other one
+"""
+
+from stanfordkarel import *
+
+
+def main():
+    while front_is_clear():
+        move_to_wall()
+        turn_if_possible()
+
+
+def turn_if_possible():
+    """
+    Pre: front is not clear
+    Post: front is clear, if possible
+    """
+    if left_is_clear():
+        turn_left()
+    if right_is_clear():
+        turn_right()
+
+
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+
+def move_to_wall():
+    while front_is_clear():
+        move()
+
+
+if __name__ == "__main__":
+    """Run python from the current directory, see worlds directory"""
+    run_karel_program("labyrinth")
---
+++ cip/w2/examples/p1_problem_solving/worlds/labyrinth.w
@@ -0,0 +1,14 @@
+Dimension: (5, 4)
+Wall: (2, 1); east
+Wall: (3, 1); north
+Wall: (4, 1); north
+Wall: (1, 2); east
+Wall: (2, 2); east
+Wall: (4, 2); north
+Wall: (5, 2); north
+Wall: (1, 3); east
+Wall: (3, 3); east
+Wall: (2, 3); north
+Wall: (3, 3); north
+Wall: (4, 3); north
+Karel: (3, 1); east
