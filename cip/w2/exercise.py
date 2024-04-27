80b336c040683a94e12785c2688c39ab6328020b
Emanuele <egalli64@gmail.com>
2024-04-27 13:16:15 +0200

    review 2024

--- cip/w2/exercise.py
+++ cip/w2/exercise.py
@@ -1,16 +1,22 @@
 """
-Code in Place 2023 https://codeinplace.stanford.edu/cip3
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
 My notes: https://github.com/egalli64/pythonesque/cip
 
 Section Week 2: Spread Beepers
-- Custom world. Karel is on (1,1), facing east
+- Karel is on (1,1), facing East
 - There is a pile of beepers on (2,1), Karel has to spread them out along the row
+- Karel can't count! Each beeper has to be placed in the first available empty cell
 """
+
 from stanfordkarel import *
 
 
 def main():
-    # We know for sure the pile is on (1,1)
+    """
+    Pre: in (1, 1) facing East
+    Post: in (1, 1) facing East
+    """
+    # We know for sure the pile is on (2, 1)
     move()
     # Do the actual job
     spread()
@@ -19,7 +25,12 @@ def main():
 
 
 def spread():
-    """ Spread the beepers in the current position on the right ones"""
+    """
+    Spread the beepers in the current position on the right ones
+
+    Pre: on the pile, facing East
+    Post: on the pile, facing East
+    """
 
     while beepers_present():
         pick_beeper()
@@ -33,13 +44,22 @@ def spread():
 
 
 def move_to_first_free():
-    """Move until a free place is seen"""
+    """
+    Move ahead until a free place is seen
+
+    It is assumed there is place where to move
+    """
     while beepers_present():
         move()
 
 
 def back_to_pile():
-    """Move back to the pile"""
+    """
+    Move back to the first beeper in the row
+
+    Pre: facing East
+    Post: facing East
+    """
     turn_around()
     move_to_first_free()
     turn_around()
@@ -47,17 +67,19 @@ def back_to_pile():
 
 
 def turn_around():
-    """Utility function"""
     turn_left()
     turn_left()
 
 
 def back_home():
-    """Move from the pile (2,1) to the Karel starting position (1,1)"""
+    """
+    Pre: on (2,1) facing East
+    Post: on (1,1) facing East
+    """
     turn_around()
     move()
     turn_around()
 
 
-if __name__ == '__main__':
-    run_karel_program("cip_w2_7")
+if __name__ == "__main__":
+    run_karel_program("exercise")
--- cip/w2/worlds/exercise.w
+++ cip/w2/worlds/exercise.w
@@ -2,4 +2,3 @@ Dimension: (9, 1)
 BeeperBag: INFINITY
 Beeper: (2, 1); 7
 Karel: (1, 1); East
-Speed: 0.75
