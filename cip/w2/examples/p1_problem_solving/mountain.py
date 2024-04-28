e3b89f9b376f573d2b1bddadf7c6e0b12a58ffb1
Emanuele <egalli64@gmail.com>
2024-04-28 19:20:09 +0200

    Mountain Karel

---
+++ cip/w2/examples/p1_problem_solving/mountain.py
@@ -0,0 +1,64 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Mountain Karel: climb a mountain a put a beeper on top of it
+"""
+
+from stanfordkarel import *
+
+
+def main():
+    climb()
+    put_beeper()
+    descend()
+
+
+def climb():
+    """
+    Pre: at the mountain West base
+    Post: at the mountain top
+    """
+    while front_is_blocked():
+        step_up()
+
+
+def step_up():
+    """
+    Pre: facing a wall East
+    Post: facing East
+    """
+    turn_left()
+    move()
+    turn_right()
+    move()
+
+
+def descend():
+    """
+    Pre: at the mountain top
+    Post: at the mountain East base
+    """
+    while front_is_clear():
+        step_down()
+
+
+def step_down():
+    """
+    Pre: on a row > 1, facing East
+    Post: on the row below, facing East
+    """
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+
+if __name__ == "__main__":
+    """Run python from the current directory, see worlds directory"""
+    run_karel_program("mountain")
---
+++ cip/w2/examples/p1_problem_solving/worlds/mountain.w
@@ -0,0 +1,17 @@
+Dimension: (9, 6)
+BeeperBag: 1
+Wall: (2, 1); west
+Wall: (2, 1); north
+Wall: (8, 1); east
+Wall: (8, 1); north
+Wall: (3, 2); west
+Wall: (3, 2); north
+Wall: (7, 2); east
+Wall: (7, 2); north
+Wall: (4, 3); west
+Wall: (4, 3); north
+Wall: (6, 3); east
+Wall: (6, 3); north
+Wall: (5, 4); west
+Wall: (5, 4); north
+Wall: (5, 4); east
