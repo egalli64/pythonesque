b3109e789239cc6ecaba90ff53f99a5e378c883c
Emanuele <egalli64@gmail.com>
2024-04-29 11:30:15 +0200

    Checkerboard Karel

---
+++ cip/w2/challenges/checkerboard.py
@@ -0,0 +1,68 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Checkerboard Karel: create a checkerboard pattern
+"""
+
+from stanfordkarel import *
+
+
+def main():
+    put_beeper()
+    fill_row()
+    while left_is_clear():
+        next_row()
+        fill_row()
+    back_col()
+
+
+def fill_row():
+    while front_is_clear():
+        if beepers_present():
+            move()
+        if front_is_clear():
+            move()
+            put_beeper()
+    back_row()
+
+
+def back_row():
+    turn_around()
+    while front_is_clear():
+        move()
+    turn_around()
+
+
+def back_col():
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+
+def next_row():
+    turn_left()
+    if beepers_present():
+        move()
+    else:
+        move()
+        put_beeper()
+    turn_right()
+
+
+def turn_around():
+    for i in range(2):
+        turn_left()
+
+
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+
+if __name__ == "__main__":
+    """
+    Run python from the current directory
+    """
+    run_karel_program("8x8")
