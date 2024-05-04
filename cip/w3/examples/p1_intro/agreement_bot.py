a16462840356f1ea57eea3c45908fb96df723a9f
Emanuele <egalli64@gmail.com>
2024-05-04 13:38:54 +0200

    Agreement bot

---
+++ cip/w3/examples/p1_intro/agreement_bot.py
@@ -0,0 +1,15 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Always agree with the user about the preferred animal
+"""
+
+
+def main():
+    fav = input("What's your favorite animal? ")
+    print("My favorite animal is also " + fav + "!")
+
+
+if __name__ == "__main__":
+    main()
