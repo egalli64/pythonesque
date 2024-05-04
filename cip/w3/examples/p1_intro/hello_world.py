2f080cd0e8218203d20c628a6fe6aaa727a2da1e
Emanuele <egalli64@gmail.com>
2024-05-04 20:16:17 +0200

    Hello World

---
+++ cip/w3/examples/p1_intro/hello_world.py
@@ -0,0 +1,14 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Hello World
+"""
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
