ad792d4d298266640cb50ba4b2a1303c55aa86b5
Emanuele <egalli64@gmail.com>
2024-05-04 20:13:05 +0200

    simplify

--- cip/w3/examples/p1_intro/fahrenheit_to_celsius.py
+++ cip/w3/examples/p1_intro/fahrenheit_to_celsius.py
@@ -3,7 +3,6 @@ Code in Place 2024 https://codeinplace.stanford.edu/cip4
 My notes: https://github.com/egalli64/pythonesque/cip
 
 Convert a Fahrenheit temperature to Celsius
-spiler: degrees_celsius = (degrees_fahrenheit - 32) * 5.0/9.0
 """
 
 
