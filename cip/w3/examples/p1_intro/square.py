39d2ba2148807d25a19ade2a15703f57f35a630c
Emanuele <egalli64@gmail.com>
2024-05-04 22:38:00 +0200

    Square number

---
+++ cip/w3/examples/p1_intro/square.py
@@ -0,0 +1,15 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Square number: Ask for a number and print its square
+"""
+
+
+def main():
+    x = float(input("Type a number to see its square: "))
+    print(str(x) + " squared is " + str(x**2))
+
+
+if __name__ == "__main__":
+    main()
