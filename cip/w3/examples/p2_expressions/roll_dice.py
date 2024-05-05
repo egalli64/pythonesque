fb94227dbc448db5f0cf5d1e314e41ef68b4e74d
Emanuele <egalli64@gmail.com>
2024-05-05 12:30:54 +0200

    Roll Dice

---
+++ cip/w3/examples/p2_expressions/roll_dice.py
@@ -0,0 +1,30 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Roll Dice: roll two dice, print each roll and the total
+"""
+
+import random
+
+NUM_SIDES = 6
+
+
+def main():
+    # random.seed(1)
+
+    first = random.randint(1, NUM_SIDES)
+    second = random.randint(1, NUM_SIDES)
+
+    total = first + second
+
+    print("Dice have", NUM_SIDES, "sides each.")
+    print("First die:", first)
+    print("Second die:", second)
+    print("Total of two dice:", total)
+
+
+# This provided line is required at the end of a Python file
+# to call the main() function.
+if __name__ == "__main__":
+    main()
