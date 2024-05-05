f481c1af31ac648ac569f6f007bf17c43045a66b
Emanuele <egalli64@gmail.com>
2024-05-05 12:39:34 +0200

    Square Root

---
+++ cip/w3/examples/p2_expressions/square_root.py
@@ -0,0 +1,18 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Square Root: calculate it
+"""
+
+import math
+
+
+def main():
+    num = float(input("Enter number: "))
+    root = math.sqrt(num)
+    print("Square root of", num, "is", root)
+
+
+if __name__ == "__main__":
+    main()
