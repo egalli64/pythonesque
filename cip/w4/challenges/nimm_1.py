c72dd426705f3d2390028694819463309f872362
Emanuele <egalli64@gmail.com>
2024-05-11 22:53:42 +0200

    Nimm - Milestone 1

---
+++ cip/w4/challenges/nimm_1.py
@@ -0,0 +1,22 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Week 4: The Game of Nimm - Milestone 1
+--------------------
+Start with 20 stones. Remove stones and print out how many stones are left until there are zero
+"""
+
+
+def main():
+    stones = 20
+    while stones > 0:
+        print(f"There are {stones} stones left.")
+        taken = int(input("Would you like to remove 1 or 2 stones? "))
+        stones -= taken
+
+    print("Game over")
+
+
+if __name__ == "__main__":
+    main()
