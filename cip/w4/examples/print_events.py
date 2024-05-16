bc3b9d5fca1dec28e1cebaa693d7b0c61afd5ba4
Emanuele <egalli64@gmail.com>
2024-05-16 20:59:11 +0200

    Print Evens

---
+++ cip/w4/examples/print_events.py
@@ -0,0 +1,16 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Program: Print Evens
+Prints the first 20 even numbers
+"""
+
+
+def main():
+    for i in range(20):
+        print(i * 2)
+
+
+if __name__ == "__main__":
+    main()
