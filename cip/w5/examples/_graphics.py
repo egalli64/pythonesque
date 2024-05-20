7f29d802b74e50aa84e09773583c0b0ceba75c10
Emanuele <egalli64@gmail.com>
2024-05-20 11:11:44 +0200

    mock graphics

---
+++ cip/w5/examples/_graphics.py
@@ -0,0 +1,18 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Section Week 4: Random Circles
+- Mock for the graphic module internally used by Stanford
+"""
+
+
+class Canvas(object):
+    def __init__(self, x, y):
+        print(f"Mocking a {x, y} canvas")
+
+    def create_oval(self, x0, y0, x1, y1, color):
+        print(f"Mocking a {color} oval in {x0, y0}, {x1, y1}")
+
+    def create_rectangle(self, x0, y0, x1, y1, color):
+        print(f"Mocking a {color} rectangle in {x0, y0}, {x1, y1}")
