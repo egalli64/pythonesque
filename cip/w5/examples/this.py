a2a196f8c431716c9b73717987e2ca02918c4106
Emanuele <egalli64@gmail.com>
2024-05-21 20:44:26 +0200

    THIS big!

---
+++ cip/w5/examples/this.py
@@ -0,0 +1,29 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Program: THIS big!
+"""
+
+from _graphics import Canvas
+
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 300
+THIS_BIG = 144
+CENTER_X = 160
+CENTER_Y = 160
+
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+
+    canvas.create_rectangle(
+        CENTER_X - THIS_BIG // 2,
+        CENTER_Y - THIS_BIG // 2,
+        CENTER_X + THIS_BIG // 2,
+        CENTER_Y + THIS_BIG // 2,
+    )
+
+
+if __name__ == "__main__":
+    main()
