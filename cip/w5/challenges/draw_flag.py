efe25b31158f63d0bf606f4ddbc801cb44079d49
Emanuele <egalli64@gmail.com>
2024-05-21 21:03:16 +0200

    Draw Flag

---
+++ cip/w5/challenges/draw_flag.py
@@ -0,0 +1,20 @@
+"""
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Program: Draw Flag (Indonesia)
+"""
+
+from _graphics import Canvas
+
+CANVAS_WIDTH = 450
+CANVAS_HEIGHT = 300
+
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    canvas.create_rectangle(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT // 2, "red")
+
+
+if __name__ == "__main__":
+    main()
