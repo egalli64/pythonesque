169f8a877e58e2b769b8d4391286fd688c5817f5
Emanuele <egalli64@gmail.com>
2024-05-21 21:29:13 +0200

    review

--- cip/w5/challenges/scribble.py
+++ cip/w5/challenges/scribble.py
@@ -1,11 +1,12 @@
 """
-Code in Place 2023 https://codeinplace.stanford.edu/cip3
+Code in Place 2024 https://codeinplace.stanford.edu/cip4
 My notes: https://github.com/egalli64/pythonesque/cip
 
 Section Week 5: Scribble
 - Draw a circle wherever the mouse is located on the screen
 """
-from graphics import Canvas
+
+from _graphics import Canvas
 import time
 
 CANVAS_WIDTH = 300
@@ -17,14 +18,16 @@ DELAY = 1
 def main():
     canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
 
-    while True:
-        x = canvas.get_mouse_x()
-        y = canvas.get_mouse_y()
+    # uncomment and format to loop
+    # while True:
+    x = canvas.get_mouse_x()
+    y = canvas.get_mouse_y()
 
-        write_circle(canvas, x, y, 'cyan')
+    write_circle(canvas, x, y, "cyan")
 
-        canvas.mainloop()
-        time.sleep(DELAY)
+    # uncomment for activate the main loop
+    # canvas.mainloop()
+    # time.sleep(DELAY)
 
 
 def write_circle(canvas, x, y, color):
