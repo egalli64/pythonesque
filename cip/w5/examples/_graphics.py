1307ea347b2981e6fadce9ff4b16203fd68ce0d2
Emanuele <egalli64@gmail.com>
2024-05-21 11:27:33 +0200

    mock image

--- cip/w5/examples/_graphics.py
+++ cip/w5/examples/_graphics.py
@@ -9,16 +9,19 @@ Section Week 4: Random Circles
 
 class Canvas(object):
     def __init__(self, x, y):
-        print(f"Mocking a {x, y} canvas")
+        print(f"Mock {x, y} canvas")
 
     def create_oval(self, x0, y0, x1, y1, color):
-        print(f"Mocking a {color} oval in {x0, y0}, {x1, y1}")
+        print(f"Mock {color} oval in {x0, y0}, {x1, y1}")
 
-    def create_rectangle(self, x0, y0, x1, y1, color):
-        print(f"Mocking a {color} rectangle in {x0, y0}, {x1, y1}")
+    def create_rectangle(self, x0, y0, x1, y1, color="black"):
+        print(f"Mock {color} rectangle in {x0, y0}, {x1, y1}")
 
     def create_line(self, x0, y0, x1, y1):
-        print(f"Mocking a line from {x0, y0} to {x1, y1}")
+        print(f"Mock line from {x0, y0} to {x1, y1}")
 
     def create_text(self, x, y, text, font, font_size, color):
-        print(f"Mocking a line from {x, y} for '{text}', font {font} {font_size} {color}")
+        print(f"Mock text from {x, y} for '{text}', font {font} {font_size} {color}")
+
+    def create_image(self, x, y, filename):
+        print(f"Mock image from {x, y} for {filename}")
