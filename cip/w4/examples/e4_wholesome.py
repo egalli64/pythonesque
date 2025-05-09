02a7443dcdad181c91b91a186b5964483fda17a4
Emanuele <egalli64@gmail.com>
2025-05-09 16:07:40 +0200

    cip5 review

--- cip/w4/examples/e4_wholesome.py
+++ cip/w4/examples/e4_wholesome.py
@@ -1,5 +1,5 @@
 """
-Code in Place 2024 https://codeinplace.stanford.edu/cip4
+Code in Place 2025 https://codeinplace.stanford.edu/cip5
 My notes: https://github.com/egalli64/pythonesque/cip
 
 Wholesome machine: check if the user input the expected sentence
---
+++ cip/w4/examples/e6_print_1_10.py
@@ -0,0 +1,16 @@
+"""
+Code in Place 2025 https://codeinplace.stanford.edu/cip5
+My notes: https://github.com/egalli64/pythonesque/cip
+
+Program: Practice
+Print the first 10 numbers in order, using a for loop
+"""
+
+
+def main():
+    for i in range(1, 11):
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
