59cd221e8defd3b956f3735ead53b5f95cbb9fdb
Emanuele <egalli64@gmail.com>
2026-05-08 16:39:53 +0200

    Hello pygame

---
+++ pygame/README.md
@@ -0,0 +1,6 @@
+# pythonesque pygame
+
+Install pygame: pip install pygame
+Run a sample game: python -m pygame.examples.aliens
+
+Tutorials: https://www.pygame.org/wiki/tutorials
---
+++ pygame/hello-pygame.py
@@ -0,0 +1,65 @@
+"""
+Pygame quickstart: https://www.pygame.org/docs/
+My notes: https://github.com/egalli64/pythonesque/pygame
+
+Example file showing a circle moving on screen
+"""
+
+import pygame
+
+# screen and circle colors
+BACKGROUND = "purple"
+FOREGROUND = "red"
+
+# pygame setup
+pygame.init()
+
+# set the program window
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Move the circle (wasd)")
+
+# used to keep an eye on the FPS
+clock = pygame.time.Clock()
+
+# no initial movement (just for code clarity)
+delta_time = 0
+
+# set the starting circle position in the middle of the screen
+position = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+print("Ready.")
+
+running = True
+while running:
+    # poll for events
+    for event in pygame.event.get():
+        # pygame.QUIT event means the user clicked X to close your window
+        if event.type == pygame.QUIT:
+            running = False
+
+    # clean the screen with the background color
+    screen.fill(BACKGROUND)
+    # place the circle in the current position on the screen
+    pygame.draw.circle(screen, FOREGROUND, position, 40)
+
+    # we get in keys a frozen snapshot of the current keyboard state
+    # change the circle position if any key in "w s a d" is pressed 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        position.y -= 300 * delta_time
+    if keys[pygame.K_s]:
+        position.y += 300 * delta_time
+    if keys[pygame.K_a]:
+        position.x -= 300 * delta_time
+    if keys[pygame.K_d]:
+        position.x += 300 * delta_time
+
+    # put the changes on screen (in one shot)
+    pygame.display.flip()
+
+    # try to keep FPS = 60 (smooth animation)
+    # dt is delta time in seconds since last frame: (framerate-independent movement)
+    delta_time = clock.tick(60) / 1000
+
+print("Terminating.")
+pygame.quit()
