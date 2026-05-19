c6af5d3f78cd85663eeac5666ebbb7bf7af33b5c
Emanuele <egalli64@gmail.com>
2026-05-19 18:31:52 +0200

    Event logger

---
+++ pygame/beginning/c3/2.py
@@ -0,0 +1,25 @@
+"""
+Event logger
+
+From: Beginnning Python Games Development with PyGame - https://link.springer.com/book/10.1007/978-1-4842-0970-7
+My reviewed version: https://github.com/egalli64/pythonesque/pygame/beginning
+"""
+
+import pygame
+
+pygame.init()
+SCREEN_SIZE = (800, 600)
+screen = pygame.display.set_mode(SCREEN_SIZE)
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            running = False
+
+        # in any case print the current event
+        print(event)
+
+        # notice: there is no need here of flipping the buffer
