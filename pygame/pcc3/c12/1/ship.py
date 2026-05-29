0bd1c7a994adab318cf1d0057db6c3f598f20024
Emanuele <egalli64@gmail.com>
2026-05-29 10:52:42 +0200

    ship /1

---
+++ pygame/pcc3/c12/1/ship.py
@@ -0,0 +1,28 @@
+"""
+Python Crash Course, Third Edition, Part II https://nostarch.com/python-crash-course-3rd-edition
+My notes: https://github.com/egalli64/pythonesque/pygame/pcc3
+
+Chapter 12 - A Ship that fires bullets
+Starting the Game Project
+"""
+
+import pygame
+
+
+class Ship:
+    """A class to manage the ship."""
+
+    def __init__(self, screen):
+        """Initialize the ship and set its starting position."""
+        self.screen = screen
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load("../images/ship.bmp")
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = screen.get_rect().midbottom
+
+    def blit(self):
+        """Draw the ship at its current location in the injected screen"""
+        self.screen.blit(self.image, self.rect)
