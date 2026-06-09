7d37458733cb71a2e7a7d471d42c253ad9dc74a5
Emanuele <egalli64@gmail.com>
2026-06-09 15:55:11 +0200

    Using locally installed fonts

---
+++ pygame/adams/2/7/4.py
@@ -0,0 +1,57 @@
+"""
+Introduction to Pygame-ce by Ralf Adams - https://github.com/adamsralf/pygame_book/
+
+My version: https://github.com/egalli64/pythonesque/ pygame/adams folder
+
+Using locally installed fonts
+"""
+
+import pygame
+
+WIN_RECT = pygame.Rect(0, 0, 600, 100)
+TITLE = "Local font"
+# see https://www.fontsquirrel.com/fonts/list/tag/historical
+FONT_NAME = "./font/rothenbg.ttf"
+FONT_SIZE = 24
+
+BACKGROUND_COLOR = "white"
+TEXT_COLOR = "black"
+
+FPS = 30
+
+
+def main():
+    pygame.init()
+    window = pygame.Window(TITLE, WIN_RECT.size)
+
+    screen = window.get_surface()
+    clock = pygame.time.Clock()
+    all_sprites = pygame.sprite.Group()
+
+    font = pygame.font.Font(FONT_NAME, FONT_SIZE)
+    text = "This is an example of printing text using a locally installed font"
+
+    running = True
+    while running:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
+        all_sprites.update()
+        screen.fill(BACKGROUND_COLOR)
+        # Render and center the text
+        text_surface = font.render(text, True, TEXT_COLOR)
+        text_rect = text_surface.get_rect(center=(WIN_RECT.center))
+        screen.blit(text_surface, text_rect)
+        all_sprites.draw(screen)
+        window.flip()
+
+
+if __name__ == "__main__":
+    main()
+    pygame.quit()
