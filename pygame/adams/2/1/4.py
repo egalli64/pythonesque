e58a74657d080666d916a72025078772f991ef1c
Emanuele <egalli64@gmail.com>
2026-06-10 10:55:16 +0200

    refactor event loop to nested function

--- pygame/adams/2/1/4.py
+++ pygame/adams/2/1/4.py
@@ -10,49 +10,47 @@ import pygame
 
 FPS = 30
 
-TITLE_1 = "Main Window"
-TITLE_2 = "Side Window"
-
 WIN_SIZE = (300, 100)
-WIN_POS_1 = (500, 50)
-WIN_POS_2 = (820, 50)
+WIN_POSS = ((500, 50), (820, 50))
+TITLES = ("Main Window", "Side Window")
 BACKGROUND_COLORS = ((0, 255, 0), (255, 0, 0))
 
 
 def main():
-    pygame.init()
     windows = {
-        pygame.Window(size=WIN_SIZE, title=TITLE_1, position=WIN_POS_1),
-        pygame.Window(size=WIN_SIZE, title=TITLE_2, position=WIN_POS_2),
+        pygame.Window(TITLES[0], WIN_SIZE, WIN_POSS[0]),
+        pygame.Window(TITLES[1], WIN_SIZE, WIN_POSS[1]),
     }
 
-    background_colors = {}
-    for window, color in zip(windows, BACKGROUND_COLORS):
-        background_colors[window.id] = color
-
-    clock = pygame.time.Clock()
-
-    running = True
-    while running:
+    def handle_events() -> bool:
+        """Run the event loops, return False in case of termination request"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 print("Closing last window:", event)
-                running = False
+                return False
             elif event.type == pygame.WINDOWCLOSE:
                 print("Close request from window", event.window.id)
                 event.window.hide()
                 windows.discard(event.window)
+        return True
 
-        if running:
-            for window in windows:
-                screen = window.get_surface()
-                screen.fill(background_colors[window.id])
-                window.flip()
-            clock.tick(FPS)
+    clock = pygame.time.Clock()
+    background_colors = {w.id: c for w, c in zip(windows, BACKGROUND_COLORS)}
 
-    pygame.quit()
+    while handle_events():
+        clock.tick(FPS)
+
+        for window in windows:
+            screen = window.get_surface()
+            screen.fill(background_colors[window.id])
+            window.flip()
 
 
 if __name__ == "__main__":
-    main()
-    print("Done.")
+    pygame.init()
+
+    try:
+        main()
+    finally:
+        pygame.quit()
+        print("Done.")
