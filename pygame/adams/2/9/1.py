7ecd8b8fdfd0b797f4b627c587269318e55ad6f9
Emanuele <egalli64@gmail.com>
2026-06-11 11:44:52 +0200

    minor review

--- pygame/adams/2/9/1.py
+++ pygame/adams/2/9/1.py
@@ -9,14 +9,14 @@ The need of having a break
 from typing import Tuple, override
 import pygame
 
-WINDOW = pygame.Rect(0, 0, 700, 200)
+WIN_RECT = pygame.Rect(0, 0, 700, 200)
 FPS = 30
 TITLE = "Bugged continous fire"
 
 
 class Enemy(pygame.sprite.Sprite):
     MIN_X = 10
-    MAX_X = WINDOW.right - 10
+    MAX_X = WIN_RECT.right - 10
     START_POS = (MIN_X, 10)
     SPEED = pygame.Vector2(150, 0)
 
@@ -36,19 +36,20 @@ class Enemy(pygame.sprite.Sprite):
 
 
 class Bullet(pygame.sprite.Sprite):
+    MAX_Y = WIN_RECT.bottom - 30
 
-    def __init__(self, filename: str, startpos: Tuple) -> None:
+    def __init__(self, filename: str, pos: Tuple) -> None:
         super().__init__()
         self.image = pygame.image.load(filename).convert_alpha()
         self.rect: pygame.FRect = pygame.FRect(self.image.get_rect())
-        self.rect.center = startpos
+        self.rect.center = pos
         self.direction = 1
         self.speed = pygame.math.Vector2(0, 100)
 
     @override
     def update(self, dt) -> None:
         self.rect.move_ip(self.speed * dt * self.direction)
-        if self.rect.top > WINDOW.bottom - 30:
+        if self.rect.top > Bullet.MAX_Y:
             self.kill()
 
 
@@ -58,7 +59,7 @@ class Game(object):
     BACKGROUND_COLOR = (200, 200, 200)
 
     def __init__(self) -> None:
-        self.window = pygame.Window(size=WINDOW.size, title=TITLE)
+        self.window = pygame.Window(TITLE, WIN_RECT.size)
         self.screen = self.window.get_surface()
         self.clock = pygame.time.Clock()
 
