fb82382347b2cdf5b035816fcb45b48a36f37e21
Emanuele <egalli64@gmail.com>
2026-06-24 15:10:25 +0200

    animation

--- pygame/clear-code/2/player.py
+++ pygame/clear-code/2/player.py
@@ -8,20 +8,46 @@ Google Drive: https://drive.google.com/drive/folders/1WBhwu1yAzgmNwQ2w-SI6G8hzqw
 My version: https://github.com/egalli64/pythonesque/ pygame/clear-code folder
 """
 
+from enum import Enum, auto
+from os import walk
+from os.path import join
+
 import pygame
 
 
+class Direction(Enum):
+    LEFT = auto()
+    RIGHT = auto()
+    UP = auto()
+    DOWN = auto()
+
+    def __str__(self) -> str:
+        return self.name.lower()
+
+
 class Player(pygame.sprite.Sprite):
-    FILENAME = "images/player/down/0.png"
+    PATHNAME = "images/player"
     SPEED = 500
+    ANIMATION_SPEED = 10
 
     @classmethod
     def load_resources(cls):
-        cls._image = pygame.image.load(Player.FILENAME).convert_alpha()
+        cls._frames = {}
+        for direction in Direction:
+            for folder_path, _, file_names in walk(join(cls.PATHNAME, str(direction))):
+                for file_name in file_names:
+                    full_path = join(folder_path, file_name)
+                    surf = pygame.image.load(full_path).convert_alpha()
+                    cls._frames.setdefault(direction, []).append(surf)
+
+        print(cls._frames)
 
     def __init__(self, pos, obstacles):
         super().__init__()
-        self.image = Player._image
+        self.state: Direction = Direction.DOWN
+        print(self.state)
+        self.frame_index = 0
+        self.image = Player._frames[self.state][self.frame_index]
         self.rect: pygame.FRect = self.image.get_frect(center=pos)
 
         self.direction = pygame.Vector2()
@@ -34,6 +60,11 @@ class Player(pygame.sprite.Sprite):
         self.direction.update(x, y)
         if self.direction:
             self.direction.normalize_ip()
+
+        if self.direction.y != 0:
+            self.state = Direction.DOWN if self.direction.y > 0 else Direction.UP
+        elif self.direction.x != 0:
+            self.state = Direction.RIGHT if self.direction.x > 0 else Direction.LEFT
 
     def horizontal_collision(self):
         for obstacle in self.obstacles:
@@ -52,8 +83,14 @@ class Player(pygame.sprite.Sprite):
                     self.hitbox_rect.bottom = obstacle.rect.top
 
     def update(self, dt):
+        # move
         self.hitbox_rect.x += self.direction.x * self.speed * dt
         self.horizontal_collision()
         self.hitbox_rect.y += self.direction.y * self.speed * dt
         self.vertical_collision()
         self.rect.center = self.hitbox_rect.center
+
+        # choose image
+        self.frame_index = self.frame_index + 5 * dt if self.direction else 0
+        i = int(self.frame_index) % len(self._frames[self.state])
+        self.image = self._frames[self.state][i]
