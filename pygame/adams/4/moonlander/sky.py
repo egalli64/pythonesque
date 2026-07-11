ce11236d71a0bf8a1307df25b02cf1cb3b90c27f
Emanuele <egalli64@gmail.com>
2026-07-11 13:35:07 +0200

    sky as container of stars

--- pygame/adams/4/moonlander/sky.py
+++ pygame/adams/4/moonlander/sky.py
@@ -13,24 +13,43 @@ import pygame
 class Sky:
     N_STAR = 20
 
-    def __init__(self, rect) -> None:
+    def __init__(self, rect: pygame.Rect) -> None:
         self.rect = rect
         self.stars = []
         for _ in range(Sky.N_STAR):
-            self.stars.append({"pos": (randint(2, rect.right - 1), randint(2, rect.right - 1)),
-                               "size": randint(1, 3),
-                               "duration": randint(400, 800),
-                               "counter": 0,
-                               "color": randint(10, 255)})
+            self.stars.append(Star(rect))
 
     def update(self) -> None:
         for star in self.stars:
-            star["counter"] = (star["counter"] + 1) % (star["duration"] + 1)
-            if star["counter"] == 0:
-                star["color"] = (star["color"] + randint(0, 70)) % 256
-                star["size"] = (star["size"] + 1) % 4
+            star.update()
 
-    def draw(self, screen) -> None:
+    def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.rect(screen, "black", self.rect)
         for star in self.stars:
-            pygame.draw.circle(screen, (255, 255, star["color"]), star["pos"], star["size"])
+            star.draw(screen)
+
+
+class Star:
+    BOTTOM_DELTA = 50
+    BORDER_DELTA = 2
+    SIZE_INTERVAL = (1, 4)
+    DURATION_INTERVAL = (400, 600)
+    BLUE_INTERVAL = (10, 255)
+    BLUE_DELTA = 10
+
+    def __init__(self, rect: pygame.Rect) -> None:
+        x = randint(Star.BORDER_DELTA, rect.right - Star.BORDER_DELTA)
+        y = randint(Star.BORDER_DELTA, rect.bottom - Star.BOTTOM_DELTA)
+        self.pos = (x, y)
+        self.size = randint(*Star.SIZE_INTERVAL)
+        self.color = (255, 255, randint(*Star.BLUE_INTERVAL))
+        self.duration = randint(*Star.DURATION_INTERVAL)
+        self.counter = 0
+
+    def update(self) -> None:
+        self.counter = (self.counter + 1) % self.duration
+        if self.counter == 0:
+            self.size = randint(*Star.SIZE_INTERVAL)
+
+    def draw(self, screen: pygame.Surface) -> None:
+        pygame.draw.circle(screen, self.color, self.pos, self.size)
