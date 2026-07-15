9d4cb85cd8a5da6a4aca0be541bfc4dcec3191e1
Emanuele <egalli64@gmail.com>
2026-07-15 10:19:33 +0200

    review

--- pygame/adams/2/6/3.py
+++ pygame/adams/2/6/3.py
@@ -5,104 +5,117 @@ My version: https://github.com/egalli64/pythonesque/ pygame/adams folder
 
 Polling key status - key.get_pressed()
 """
-
 from enum import Enum
 from typing import override
 import pygame
 
-WIN_RECT = pygame.Rect(0, 0, 300, 600)
+TITLE = "Keyboard to move"
+WIN_SIZE = (300, 500)
+WIN_POS = (50, 50)
 
 
 class Defender(pygame.sprite.Sprite):
     class Direction(Enum):
-        STOP = pygame.Vector2(0, 0)
         RIGHT = pygame.Vector2(1, 0)
         LEFT = pygame.Vector2(-1, 0)
         UP = pygame.Vector2(0, -1)
         DOWN = pygame.Vector2(0, 1)
+        STOP = pygame.Vector2(0, 0)
 
     class Speed(Enum):  # pixel/second
         SLOW = 10
         NORMAL = 100
         FAST = 300
 
-    IMAGE = "../images/defender.png"
+    FILENAME = "../images/defender.png"
     SIZE = (30, 30)
 
-    def __init__(self) -> None:
+    image: pygame.Surface
+    rect: pygame.FRect
+
+    @classmethod
+    def load_resources(cls):
+        image = pygame.image.load(cls.FILENAME).convert_alpha()
+        cls._image = pygame.transform.scale(image, cls.SIZE)
+
+    def __init__(self, viewport: pygame.Rect) -> None:
         super().__init__()
-        self.image = pygame.image.load(Defender.IMAGE).convert_alpha()
-        self.image = pygame.transform.scale(self.image, Defender.SIZE)
-        self.rect: pygame.FRect = pygame.FRect(self.image.get_rect())
-        self.rect.center = WIN_RECT.center
+
+        self.image = Defender._image
+        self.rect = pygame.FRect(self.image.get_rect())
+        self.rect.center = viewport.center
+
+        self.viewport = viewport
         self.speed = Defender.Speed.NORMAL
         self.direction = Defender.Direction.STOP
 
-    def set_direction(self, direction: Direction):
+    def set_direction(self, direction: Direction) -> None:
         self.direction = direction
 
-    def set_speed(self, speed: Speed):
+    def set_speed(self, speed: Speed) -> None:
         self.speed = speed
 
     @override
-    def update(self, dt) -> None:
+    def update(self, dt: float) -> None:
         self.rect.move_ip(self.direction.value * self.speed.value * dt)
-        self.rect.clamp_ip(WIN_RECT)
+        self.rect.clamp_ip(self.viewport)
 
 
-class Game(object):
+def direction_from_keys(keys: pygame.key.ScancodeWrapper) -> Defender.Direction:
+    if keys[pygame.K_LEFT]:
+        return Defender.Direction.LEFT
+    elif keys[pygame.K_RIGHT]:
+        return Defender.Direction.RIGHT
+    elif keys[pygame.K_UP]:
+        return Defender.Direction.UP
+    elif keys[pygame.K_DOWN]:
+        return Defender.Direction.DOWN
+    else:
+        return Defender.Direction.STOP
+
+
+def speed_from_keys(keys: pygame.key.ScancodeWrapper) -> Defender.Speed:
+    if keys[pygame.K_LSHIFT]:
+        return Defender.Speed.FAST
+    elif keys[pygame.K_RSHIFT]:
+        return Defender.Speed.SLOW
+    else:
+        return Defender.Speed.NORMAL
+
+
+class Game:
     FPS = 30
-    TITLE = "Keyboard"
-    WIN_POS = (10, 50)
     BACKGROUND_COLOR = "white"
 
-    def __init__(self) -> None:
-        self.window = pygame.Window(Game.TITLE, WIN_RECT.size, Game.WIN_POS)
-        self.screen = self.window.get_surface()
-        self.clock = pygame.time.Clock()
+    def __init__(self, window: pygame.Window, screen: pygame.Surface) -> None:
+        self.window = window
+        self.screen = screen
 
-        self.defender = Defender()
+        self.defender = Defender(screen.get_rect())
         self.defender_group = pygame.sprite.GroupSingle(self.defender)
+        self.running = True
 
     def run(self) -> None:
-        while self.handle_events():
-            dt = self.clock.tick(Game.FPS) / 1000
-            self.defender.update(dt)
+        clock = pygame.time.Clock()
+
+        while self.running:
+            dt = clock.tick(Game.FPS) / 1000
+
+            self.handle_events()
+            self.defender_group.update(dt)
             self.draw()
 
-    def handle_events(self) -> bool:
-        """Run the event loops, return False in case of termination request"""
+    def handle_events(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return False
+                self.running = False
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                return False
+                self.running = False
 
-        # countinous events
+        # continuous events - poll the current keyboard state once per frame
         keys = pygame.key.get_pressed()
-        self.defender.set_direction(self.get_direction(keys))
-        self.defender.set_speed(self.get_speed(keys))
-        return True
-
-    def get_direction(self, keys: pygame.key.ScancodeWrapper) -> Defender.Direction:
-        if keys[pygame.K_LEFT]:
-            return Defender.Direction.LEFT
-        elif keys[pygame.K_RIGHT]:
-            return Defender.Direction.RIGHT
-        elif keys[pygame.K_UP]:
-            return Defender.Direction.UP
-        elif keys[pygame.K_DOWN]:
-            return Defender.Direction.DOWN
-        else:
-            return Defender.Direction.STOP
-
-    def get_speed(self, keys: pygame.key.ScancodeWrapper) -> Defender.Speed:
-        if keys[pygame.K_LSHIFT]:
-            return Defender.Speed.FAST
-        elif keys[pygame.K_RSHIFT]:
-            return Defender.Speed.SLOW
-        else:
-            return Defender.Speed.NORMAL
+        self.defender.set_direction(direction_from_keys(keys))
+        self.defender.set_speed(speed_from_keys(keys))
 
     def draw(self) -> None:
         self.screen.fill(Game.BACKGROUND_COLOR)
@@ -110,11 +123,16 @@ class Game(object):
         self.window.flip()
 
 
+# noinspection DuplicatedCode
 if __name__ == "__main__":
     pygame.init()
+    pg_window = pygame.Window(TITLE, WIN_SIZE, WIN_POS)
+    pg_screen = pg_window.get_surface()
+
+    Defender.load_resources()
 
     try:
-        Game().run()
+        Game(pg_window, pg_screen).run()
     finally:
         pygame.quit()
         print("Done.")
