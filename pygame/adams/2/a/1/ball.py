96f04dca514704481ed4f14fd70c3e346420df2c
Emanuele <egalli64@gmail.com>
2026-07-18 13:37:16 +0200

    robustness

--- pygame/adams/2/a/1/ball.py
+++ pygame/adams/2/a/1/ball.py
@@ -5,7 +5,6 @@ My version: https://github.com/egalli64/pythonesque/ pygame/adams folder
 
 Mouse actions
 """
-from typing import override
 import pygame
 
 
@@ -15,6 +14,7 @@ class Ball(pygame.sprite.Sprite):
     INITIAL_SIZE_RATIO = 5
 
     image: pygame.Surface
+    scaled_image: pygame.Surface
     rect: pygame.FRect
     dirty: bool
 
@@ -23,34 +23,39 @@ class Ball(pygame.sprite.Sprite):
         cls._image = pygame.image.load(cls.FILENAME).convert_alpha()
 
     def __init__(self, viewport: pygame.Rect) -> None:
-        assert viewport.width == viewport.height, "Squared viewport expected"
         super().__init__()
 
-        self.size = viewport.width / Ball.INITIAL_SIZE_RATIO
         self.viewport = viewport
-        self.reset()
+        self.max_size = min(viewport.width, viewport.height)
+        self.size = self.max_size // Ball.INITIAL_SIZE_RATIO
+        self.angle = 0
+
+        self.rebuild()
         self.rect.center = viewport.center
 
-    def reset(self) -> None:
-        self.image = pygame.transform.scale(Ball._image, (self.size, self.size))
-        self.rect = pygame.FRect(self.image.get_rect())
-        self.dirty = False
+    def rebuild(self):
+        self.update_scaled_image()
+        self.update_image()
 
-    @override
-    def update(self, pos: tuple[int, int]) -> None:
-        if self.dirty:
-            self.reset()
+    def update_scaled_image(self) -> None:
+        self.scaled_image = pygame.transform.scale(Ball._image, (self.size, self.size))
+        self.rect = pygame.FRect(self.scaled_image.get_rect())
+        self.rect.clamp_ip(self.viewport)
+
+    def update_image(self) -> None:
+        self.image = pygame.transform.rotate(self.scaled_image, self.angle)
+
+    def move(self, pos: tuple[int, int]) -> None:
         if self.rect.center != pos:
             self.rect.center = pos
             self.rect.clamp_ip(self.viewport)
 
     def rotate(self, counterclockwise: bool) -> None:
-        self.dirty = True
-        angle = 90 * (1 if counterclockwise else -1)
-        Ball._image = pygame.transform.rotate(Ball._image, angle)
+        self.angle = (self.angle + (90 if counterclockwise else -90)) % 360
+        self.update_image()
 
     def resize(self, delta: int) -> None:
         candidate = self.size + delta
-        if Ball.MIN_SIZE <= candidate <= self.viewport.width:
+        if Ball.MIN_SIZE <= candidate <= self.max_size:
             self.size = candidate
-            self.dirty = True
+            self.rebuild()
