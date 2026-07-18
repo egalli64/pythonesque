e1722ea68114cec1d2e01b38f47bd682fb60cd48
Emanuele <egalli64@gmail.com>
2026-07-18 12:07:39 +0200

    review

--- pygame/adams/2/a/1/game.py
+++ pygame/adams/2/a/1/game.py
@@ -18,54 +18,65 @@ class Game:
     INNER_BORDER_COLOR = "red"
     FPS = 30
 
-    def __init__(self) -> None:
-        self.window = pygame.Window(Game.TITLE, WIN_SIZE)
-        self.screen = self.window.get_surface()
-        self.clock = pygame.time.Clock()
+    def __init__(self, window: pygame.Window, screen: pygame.Surface) -> None:
+        self.window = window
+        self.screen = screen
+        self.running = True
 
         self.ball = Ball(Game.INNER_RECT)
+        self.group = pygame.sprite.GroupSingle(self.ball)
 
     def run(self) -> None:
-        while self.handle_events():
-            self.clock.tick(Game.FPS)
+        clock = pygame.time.Clock()
+
+        while self.running:
+            clock.tick(Game.FPS)
+
+            self.handle_events()
             self.update()
             self.draw()
 
-    def handle_events(self) -> bool:
+    def handle_events(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return False
+                self.running = False
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                return False
+                self.running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 match event.button:
                     case 1:  # LEFT
-                        self.ball.rotate(True)
+                        self.ball.rotate(counterclockwise=True)
                     case 2:  # MIDDLE
-                        return False
+                        self.running = False
                     case 3:  # RIGHT
-                        self.ball.rotate(False)
+                        self.ball.rotate(counterclockwise=False)
             elif event.type == pygame.MOUSEWHEEL:  # previously known as button 4 and 5
                 self.ball.resize(event.y)
-        return True
 
-    def update(self):
+    def update(self) -> None:
         pos = pygame.mouse.get_pos()
-        pygame.mouse.set_visible(not Game.INNER_RECT.collidepoint(pos))
-        self.ball.update(pos)
+        if Game.INNER_RECT.collidepoint(pos):
+            pygame.mouse.set_visible(False)
+            self.ball.update(pos)
+        else:
+            pygame.mouse.set_visible(True)
 
     def draw(self) -> None:
         self.screen.fill(self.BACKGROUND_COLOR)
         pygame.draw.rect(self.screen, Game.INNER_BORDER_COLOR, Game.INNER_RECT, 1)
-        self.ball.draw(self.screen)
+        self.group.draw(self.screen)
         self.window.flip()
 
 
 if __name__ == "__main__":
     pygame.init()
+    pg_window = pygame.Window(Game.TITLE, WIN_SIZE)
+    pg_screen = pg_window.get_surface()
+
+    Ball.load_resources()
 
     try:
-        Game().run()
+        Game(pg_window, pg_screen).run()
     finally:
         pygame.quit()
         print("Done.")
