8ab9c2ce2da2c548bfeee4d10714bc4ddaa14db5
Emanuele <egalli64@gmail.com>
2026-07-19 11:16:46 +0200

    review

--- pygame/adams/2/a/2.py
+++ pygame/adams/2/a/2.py
@@ -5,19 +5,15 @@ My version: https://github.com/egalli64/pythonesque/ pygame/adams folder
 
 Double click - simple but platform dependent
 """
-
 import pygame
 
 FPS = 30
-
-WIN_RECT = pygame.Rect(0, 0, 400, 100)
+WIN_SIZE = (400, 100)
 TITLE = "Double click me"
 BACKGROUND_COLOR = "white"
 
 
-def main():
-    window = pygame.Window(TITLE, WIN_RECT.size)
-    screen = window.get_surface()
+def main(window: pygame.Window, screen: pygame.Surface) -> None:
     clock = pygame.time.Clock()
 
     running = True
@@ -25,14 +21,18 @@ def main():
         clock.tick(FPS)
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:  # LEFT
-                    if event.clicks == 1:
-                        print("Single click")
-                    elif event.clicks == 2:
-                        print("Double click")
+            match event.type:
+                case pygame.QUIT:
+                    running = False
+                case pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        running = False
+                case pygame.MOUSEBUTTONDOWN:
+                    if event.button == pygame.BUTTON_LEFT:
+                        if event.clicks == 1:
+                            print("Single click")
+                        elif event.clicks >= 2:
+                            print(f"Double click (or more: {event.clicks} clicks detected)")
 
         screen.fill(BACKGROUND_COLOR)
         window.flip()
@@ -40,9 +40,11 @@ def main():
 
 if __name__ == "__main__":
     pygame.init()
+    pg_window = pygame.Window(TITLE, WIN_SIZE)
+    pg_screen = pg_window.get_surface()
 
     try:
-        main()
+        main(pg_window, pg_screen)
     finally:
         pygame.quit()
         print("Done.")
