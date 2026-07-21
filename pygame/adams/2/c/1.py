7ef21c22815c602ea56d252b264d86366a1b2706
Emanuele <egalli64@gmail.com>
2026-07-21 09:00:08 +0200

    review

--- pygame/adams/2/c/1.py
+++ pygame/adams/2/c/1.py
@@ -5,45 +5,47 @@ My version: https://github.com/egalli64/pythonesque/ pygame/adams folder
 
 Printing events
 """
-
 import pygame
+
+WIN_SIZE = (600, 300)
+TITLE = "Printing events"
+BACKGROUND_COLOR = "white"
+FPS = 30
 
 
 class Game:
-    WIN_RECT = pygame.Rect(0, 0, 600, 300)
-    TITLE = "Printing events"
-    BACKGROUND_COLOR = "white"
-    FPS = 30
-
-    def __init__(self) -> None:
-        pygame.init()
-        self.window = pygame.Window(Game.TITLE, Game.WIN_RECT.size)
-        self.screen = self.window.get_surface()
+    def __init__(self, window: pygame.Window, screen: pygame.Surface) -> None:
+        self.window = window
+        self.screen = screen
         self.clock = pygame.time.Clock()
         self.running = True
 
     def run(self) -> None:
-        while self.handle_events():
-            self.clock.tick(Game.FPS)
-            self.screen.fill(self.BACKGROUND_COLOR)
+        while self.running:
+            self.clock.tick(FPS)
+
+            self.handle_events()
+            self.screen.fill(BACKGROUND_COLOR)
             self.window.flip()
 
-    def handle_events(self) -> bool:
+    def handle_events(self) -> None:
         for event in pygame.event.get():
-            print(event)  # Print event information
+            print(f"Event {pygame.event.event_name(event.type)} with attributes {event.dict}")
+
             if event.type == pygame.QUIT:
-                return False
+                self.running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    return False
-        return True
+                    self.running = False
 
 
 if __name__ == "__main__":
     pygame.init()
+    pg_window = pygame.Window(TITLE, WIN_SIZE)
+    pg_screen = pg_window.get_surface()
 
     try:
-        Game().run()
+        Game(pg_window, pg_screen).run()
     finally:
         pygame.quit()
         print("Done.")
