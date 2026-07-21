f31b6a22070ae6cb97f3ffd09fe339e9a42a8a34
Emanuele <egalli64@gmail.com>
2026-07-21 18:36:58 +0200

    game review

--- pygame/adams/2/c/2/game.py
+++ pygame/adams/2/c/2/game.py
@@ -20,28 +20,35 @@ BOX_COUNT = 3
 
 
 class Game:
+    all_particles: pygame.sprite.Group[Particle]
+    boxes: List[Box]
 
     def __init__(self, window: pygame.Window, screen: pygame.Surface) -> None:
         self.window = window
         self.screen = screen
         self.viewport = self.screen.get_rect()
-        self.clock = pygame.time.Clock()
+
         self.all_sprites = pygame.sprite.Group()
-        self.all_particles: pygame.sprite.Group[Particle] = pygame.sprite.Group()
+        self.all_particles = pygame.sprite.Group[Particle]()
         for _ in range(PARTICLE_COUNT):
             self.all_particles.add(Particle(self.viewport, self.all_sprites))
 
         self.button = StartButton((30, self.viewport.bottom - 30), self.all_sprites)
         self.all_boxes = pygame.sprite.Group()
-        self.boxes: List[Box] = []
+        self.boxes = []
         for i in range(BOX_COUNT):
             self.boxes.append(cur := Box(i, self.viewport))
             self.all_boxes.add(cur)
         self.all_sprites.add(self.all_boxes)
+        self.running = True
 
     def run(self) -> None:
-        while self.handle_events():
-            td = self.clock.tick(FPS) / 1000
+        clock = pygame.time.Clock()
+
+        while self.running:
+            td = clock.tick(FPS) / 1000
+
+            self.handle_events()
 
             self.button.update()
             self.all_particles.update(td)
@@ -56,13 +63,13 @@ class Game:
         xs = pygame.sprite.groupcollide(self.all_particles, self.all_boxes, True, False)
         self.boxes[0].increase(len(xs))
 
-    def handle_events(self) -> bool:
+    def handle_events(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return False
+                self.running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    return False
+                    self.running = False
                 if event.key == pygame.K_SPACE:
                     self.button.toggle_running()
             elif event.type == pygame.MOUSEBUTTONDOWN:
@@ -72,9 +79,9 @@ class Game:
                 for particle in self.all_particles:
                     particle.set_frozen(event.running)
             elif event.type == EVENT_OVERFLOW:
-                if 0 <= event.next < BOX_COUNT:
-                    self.boxes[event.next].increase(event.delta)
-        return True
+                i: int = event.next
+                if 0 <= i < BOX_COUNT:
+                    self.boxes[i].increase(event.delta)
 
 
 # noinspection DuplicatedCode
