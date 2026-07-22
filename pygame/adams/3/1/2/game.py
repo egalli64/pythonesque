7ae7dfa5bb270f4880f41f1eef65c2c5be895249
Emanuele <egalli64@gmail.com>
2026-07-22 17:34:48 +0200

    game review

--- pygame/adams/3/1/2/game.py
+++ pygame/adams/3/1/2/game.py
@@ -9,50 +9,54 @@ import pygame
 from timer import Timer
 from rock import Rock
 
-WIN_RECT = pygame.Rect(0, 0, 300, 200)
+WIN_SIZE = (300, 200)
+FPS = 30
+TITLE = "Exploding rocks"
+BACKGROUND_COLOR = "black"
 
 
 class Game(object):
-    FPS = 30
-    TITLE = "Exploding rocks"
-    BACKGROUND_COLOR = "black"
-
-    def __init__(self) -> None:
-        self.window = pygame.Window(Game.TITLE, WIN_RECT.size)
-        self.screen = self.window.get_surface()
+    def __init__(self, window: pygame.Window, screen: pygame.Surface) -> None:
+        self.window = window
+        self.screen = screen
         self.viewport = self.screen.get_rect()
-        self.clock = pygame.time.Clock()
 
         self.all_rocks = pygame.sprite.Group()
         self.timer = Timer(500)
+        self.running = True
 
     def run(self) -> None:
-        while self.handle_events():
-            self.clock.tick(Game.FPS)
+        clock = pygame.time.Clock()
+        while self.running:
+            clock.tick(FPS)
+
+            self.handle_events()
 
             if self.timer.tick():
                 self.all_rocks.add(Rock(self.viewport))
             self.all_rocks.update()
 
-            self.screen.fill(Game.BACKGROUND_COLOR)
+            self.screen.fill(BACKGROUND_COLOR)
             self.all_rocks.draw(self.screen)
             self.window.flip()
 
-    def handle_events(self) -> bool:
+    def handle_events(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return False
+                self.running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    return False
-        return True
+                    self.running = False
 
 
+# noinspection DuplicatedCode
 if __name__ == "__main__":
     pygame.init()
+    pg_window = pygame.Window(TITLE, WIN_SIZE)
+    pg_screen = pg_window.get_surface()
 
     try:
-        Game().run()
+        Game(pg_window, pg_screen).run()
     finally:
         pygame.quit()
         print("Done.")
