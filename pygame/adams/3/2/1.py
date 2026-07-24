72308be761c85da0447116f6ada58d830ef5161d
Emanuele <egalli64@gmail.com>
2026-07-24 12:53:25 +0200

    review

--- pygame/adams/3/2/1.py
+++ pygame/adams/3/2/1.py
@@ -5,61 +5,65 @@ My version: https://github.com/egalli64/pythonesque/ pygame/adams folder
 
 Extract a tile from a tileset
 """
-
 import pygame
+
+TILE = pygame.math.Vector2(32, 32)
+SHEET = (16, 12)
+WIN_SIZE = (SHEET[0] * TILE.x, SHEET[1] * TILE.y)
+TITLE = "A Meadow"
+FPS = 30
 
 
 class Meadow:
-    TITLE = "A Meadow"
-    TILE = pygame.math.Vector2(32, 32)
-    SHEET = (16, 12)
-    WIN_RECT = pygame.Rect(0, 0, SHEET[0] * TILE.x, SHEET[1] * TILE.y)
+    @classmethod
+    def load_resources(cls) -> None:
+        tiles = pygame.image.load("../images/forest-tiles.png").convert_alpha()
+        cls._image = tiles.subsurface(pygame.Rect((0, 0), TILE))
 
-    def __init__(self) -> None:
-        self.window = pygame.Window(Meadow.TITLE, Meadow.WIN_RECT.size)
-        self.screen: pygame.Surface = self.window.get_surface()
-        self.spritelib = pygame.image.load("../images/forest-tiles.png").convert_alpha()
-        self.rect = self.screen.get_frect()
-        self.clock = pygame.time.Clock()
+    def __init__(self, screen: pygame.Surface) -> None:
+        self.screen = screen
 
     def draw(self) -> None:
         self.screen.fill("black")
-        image = self.spritelib.subsurface(pygame.Rect((0, 0), Meadow.TILE))
-        for row in range(Meadow.SHEET[1]):
-            for col in range(Meadow.SHEET[0]):
-                position = col * Meadow.TILE.x, row * Meadow.TILE.y
-                self.screen.blit(image, position)
-        self.window.flip()
+        for row in range(SHEET[1]):
+            for col in range(SHEET[0]):
+                position = col * TILE.x, row * TILE.y
+                self.screen.blit(Meadow._image, position)
 
 
 class Game:
-    FPS = 30
-
-    def __init__(self) -> None:
-        self.clock = pygame.time.Clock()
-        self.window = Meadow()
+    def __init__(self, window: pygame.Window, screen: pygame.Surface) -> None:
+        self.window = window
+        self.meadow = Meadow(screen)
         self.running = True
 
     def run(self) -> None:
-        while self.handle_events():
-            self.clock.tick(Game.FPS)
-            self.window.draw()
+        clock = pygame.time.Clock()
+        while self.running:
+            clock.tick(FPS)
 
-    def handle_events(self) -> bool:
+            self.handle_events()
+            self.meadow.draw()
+            self.window.flip()
+
+    def handle_events(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return False
+                self.running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    return False
-        return True
+                    self.running = False
 
 
 if __name__ == "__main__":
     pygame.init()
+    pg_window = pygame.Window(TITLE, WIN_SIZE)
+    pg_screen: pygame.Surface = pg_window.get_surface()
+
+    Meadow.load_resources()
 
     try:
-        Game().run()
+        Game(pg_window, pg_screen).run()
     finally:
         pygame.quit()
         print("Done.")
