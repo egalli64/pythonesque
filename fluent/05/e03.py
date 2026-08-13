033502c2bf0609a22e3e315e254621e4eb04dbf2
Emanuele <egalli64@gmail.com>
2026-08-13 14:15:25 +0200

    Typed Named Tuples

---
+++ fluent/05/e03.py
@@ -0,0 +1,28 @@
+"""
+Fluent Python (Second Edition) by Luciano Ramalho
+https://www.oreilly.com/library/view/fluent-python-2nd/9781492056348/
+
+Original code: https://github.com/fluentpython/example-code-2e/
+My playground: https://github.com/egalli64/pythonesque/ fluent folder
+
+Typed Named Tuples
+"""
+from typing import NamedTuple
+
+
+class Coordinate(NamedTuple):
+    """
+    Every instance field must be annotated with a type
+    Any instance field could have a default value (following the usual rules)
+    """
+    lat: float
+    lon: float
+    reference: str = 'WGS84'
+
+
+print("a NamedTuple class is a tuple:", issubclass(Coordinate, tuple))
+
+# hints: documentation that can be verified by IDEs and type checkers
+trash = Coordinate('Ni!', None)
+# we could have a warning from IDE/type checker, but Python does not mind about them
+print(trash)
--- fluent/README.md
+++ fluent/README.md
@@ -117,3 +117,4 @@
 
 * 1: [Basic data class](05/e01.py)
 * 2: [Classic Named Tuples](05/e02.py)
+* 3: [Typed Named Tuples](05/e03.py)
